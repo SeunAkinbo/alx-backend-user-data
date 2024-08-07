4de1f2e7447eb5bd558487e059d81ce69a8c1e91
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-08-07 02:21:46 +0100

    Auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,13 +2,25 @@
 """Module - auth.py"""
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
     """The authentication class"""
     def requires_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """The required authentication method"""
-        return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_headers(self, request=None) -> str:
         """The authorization headers method"""
