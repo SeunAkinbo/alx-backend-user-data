97b104f2f39ba08f1eef0f5cf236f58e96477aec
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-08-07 01:57:40 +0100

    Auth Class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,21 +8,11 @@ class Auth:
     """The authentication class"""
     def requires_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """The required authentication method"""
-        if path is None:
-            return True
-        if excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
+        return False
 
     def authorization_headers(self, request=None) -> str:
         """The authorization headers method"""
-        if request is None:
-            return None
-        return request.headers.get('Authorization')
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """The current user method"""
