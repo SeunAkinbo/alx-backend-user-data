089670682b05c1741f30fd87267608cf1897aeed
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-08-09 01:02:35 +0100

    Empty Session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module - session_auth
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session Authentication class"""
+    pass
